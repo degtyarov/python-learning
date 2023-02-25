072b9681b79194360cf7953247b3e36bb5d89cdb
degtyarov <124345495+degtyarov@users.noreply.github.com>
2023-02-25 14:01:03 +0200

    update code
    Update code of task

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
 # r = 'Red' 
 # g = 'Green' 
 # b = 'Blue'
-print('Red','Blue','Green', 'RedGreenBlue','Blue GreenBlue',end='\n')
+print('Red','Blue','Green', 'RedGreenBlue', 'Blue GreenBlue',sep=' ',end='\n')
